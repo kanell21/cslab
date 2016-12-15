6a78b17badb1c8690e5d82d59fc7076113b68283
Christina Giannoula <christina.giann@gmail.com>
2016-12-15 21:02:27 +0200

    Add files via upload

---
+++ dijkstra/graph.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include<string.h>
+
+#include "graph.h"
+#include "minheap.h"
+
+// Read graph
+struct Graph* graph_read(const char *filename)
+{
+    FILE *fp;
+    fp = fopen(filename,"r");
+    char line[50];
+    char *token;
+    int V;
+    int dest;
+    int source;
+    int weight;
+    int counter=1;
+    struct Graph* graph;
+    int edges=0;
+	
+    while ( fgets ( line, sizeof line, fp ) != NULL ) /* read a line */
+      {
+        token=strtok(line," ");
+        //printf("Token=%s\n",token);
+
+
+        if(strcmp(token,"c")==0 && counter==2)
+        {
+			token=strtok(NULL," ");
+			// printf("Token=%s\n",token);
+			token=strtok(NULL," ");
+			// printf("Token=%s\n",token);
+			token=strtok(NULL," ");
+			//printf("Token=%s\n",token);
+			token=strtok(NULL," ");
+			// printf("Token=%s\n",token);
+			V=atoi(token);
+			// printf("V=%d",V);
+			graph = createGraph(V);
+        }
+        else if(strcmp(token,"a")==0)
+        { 
+
+			//printf("Here");
+            token=strtok(NULL," ");
+            source=atoi(token);
+            //printf("Source=%d\t",source);
+
+            token=strtok(NULL," ");
+            dest=atoi(token);
+            //printf("Dest=%d\n",dest);
+
+            token=strtok(NULL," ");
+            weight=atoi(token);
+            //printf("Weight=%d\n",weight);
+            edges++;
+			addEdge(graph, source, dest, weight);
+          
+        }
+      counter++;
+      }   
+	  
+	return graph;
+}
+
+// A utility function to create a new adjacency list node
+struct AdjListNode* newAdjListNode(int dest, int weight)
+{
+    struct AdjListNode* newNode =
+            (struct AdjListNode*) malloc(sizeof(struct AdjListNode));
+    newNode->dest = dest;
+    newNode->weight = weight;
+    return newNode;
+}
+ 
+// A utility function that creates a graph of V vertices
+struct Graph* createGraph(int V)
+{
+    struct Graph* graph = (struct Graph*) malloc(sizeof(struct Graph));
+    graph->V = V;
+ 
+    // Create an array of adjacency lists.  Size of array will be V
+    graph->array = (struct AdjList*) malloc(V * sizeof(struct AdjList));
+ 
+     // Initialize each adjacency list as empty by making head as NULL
+    for (int i = 0; i < V; ++i){      
+      graph->array[i].head = NULL;
+      graph->array[i].neighboors = 0;
+	}
+ 
+    return graph;
+}
+ 
+// Adds an edge to an undirected graph
+void addEdge(struct Graph* graph, int src, int dest, int weight)
+{
+    // Add an edge from src to dest.  A new node is added to the adjacency
+    // list of src.  The node is added at the begining
+    struct AdjListNode* newNode = newAdjListNode(dest, weight);
+    if(graph->array[src].head==NULL) 
+		graph->array[src].head = (struct AdjListNode *) malloc(sizeof(struct AdjListNode));
+    else 
+		graph->array[src].head = (struct AdjListNode *) realloc(graph->array[src].head, (graph->array[src].neighboors+1) * sizeof(struct AdjListNode));
+    
+	graph->array[src].head[graph->array[src].neighboors] = *newNode;
+    graph->array[src].neighboors++;
+ 
+    // Since graph is undirected, add an edge from dest to src also
+    newNode = newAdjListNode(src, weight);
+    
+	if(graph->array[dest].head==NULL) 
+		graph->array[dest].head = (struct AdjListNode *) malloc(sizeof(struct AdjListNode));
+    else 
+		graph->array[dest].head = (struct AdjListNode *) realloc(graph->array[dest].head, (graph->array[dest].neighboors+1) * sizeof(struct AdjListNode));
+    
+	graph->array[dest].head[graph->array[dest].neighboors] = *newNode;
+    graph->array[dest].neighboors++;
+}
---
+++ dijkstra/minheap.c
@@ -0,0 +1,144 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "minheap.h"
+#include "dijkstra.h"
+
+/*
+ * Global vars
+ */
+extern struct node_chars *nodes;
+
+// A utility function to create a new Min Heap Node
+struct MinHeapNode* newMinHeapNode(int v, int dist)
+{
+    struct MinHeapNode* minHeapNode =
+           (struct MinHeapNode*) malloc(sizeof(struct MinHeapNode));
+    minHeapNode->v = v;
+    minHeapNode->dist = dist;
+    return minHeapNode;
+}
+ 
+// A utility function to create a Min Heap
+struct MinHeap* createMinHeap(int capacity)
+{
+    struct MinHeap* minHeap =
+         (struct MinHeap*) malloc(sizeof(struct MinHeap));
+    minHeap->size = 0;
+    minHeap->capacity = capacity;
+    minHeap->array =
+         (struct MinHeapNode**) malloc(capacity * sizeof(struct MinHeapNode*));
+	minHeap->ups_decrease_key = 0;
+	minHeap->decrease_keys = 0;
+    return minHeap;
+}
+ 
+// A utility function to swap two nodes of min heap. Needed for min heapify
+void swapMinHeapNode(struct MinHeapNode** a, struct MinHeapNode** b)
+{
+    struct MinHeapNode* t = *a;
+    *a = *b;
+    *b = t;
+}
+ 
+// A standard function to heapify at given idx
+// This function also updates position of nodes when they are swapped.
+// Position is needed for decreaseKey()
+void minHeapify(struct MinHeap* minHeap, int idx)
+{
+    int smallest, left, right;
+    smallest = idx;
+    left = 2 * idx + 1;
+    right = 2 * idx + 2;
+    struct MinHeapNode *smallestNode;
+    struct MinHeapNode *idxNode ;
+	
+    if (left < minHeap->size &&
+        minHeap->array[left]->dist < minHeap->array[smallest]->dist )
+      smallest = left;
+ 
+    if (right < minHeap->size &&
+        minHeap->array[right]->dist < minHeap->array[smallest]->dist )
+      smallest = right;
+ 
+    if (smallest != idx)
+    {
+        // The nodes to be swapped in min heap        
+        smallestNode=minHeap->array[smallest];
+        idxNode= minHeap->array[idx];
+ 
+        // Swap positions
+        nodes[smallestNode->v].pos = idx;
+        nodes[idxNode->v].pos = smallest;
+ 
+        // Swap nodes
+        swapMinHeapNode(&minHeap->array[smallest], &minHeap->array[idx]);
+ 
+        minHeapify(minHeap, smallest);
+    }
+}
+ 
+// A utility function to check if the given minHeap is ampty or not
+int isEmpty(struct MinHeap* minHeap)
+{
+    return minHeap->size == 0;
+}
+ 
+// Standard function to extract minimum node from heap
+struct MinHeapNode* extractMin(struct MinHeap* minHeap)
+{
+    if (isEmpty(minHeap))
+        return NULL;
+ 
+    // Store the root node
+    struct MinHeapNode* root = minHeap->array[0];
+ 
+    // Replace root node with last node
+    struct MinHeapNode* lastNode = minHeap->array[minHeap->size - 1];
+    minHeap->array[0] = lastNode;
+ 
+    // Update position of last node
+    nodes[root->v].pos = minHeap->size-1;
+    nodes[lastNode->v].pos = 0;
+ 
+    // Reduce heap size and heapify root
+    --minHeap->size;
+    minHeapify(minHeap, 0);
+ 
+    return root;
+}
+
+// Function to decrease dist value of a given vertex v. This function
+// uses pos[] of min heap to get the current index of node in min heap
+void decreaseKey(struct MinHeap* minHeap, int v, int dist)
+{
+    // Get the index of v in  heap array
+    int i = nodes[v].pos;
+    minHeap->decrease_keys++;
+	
+    // Get the node and update its dist value
+    minHeap->array[i]->dist = dist;
+ 
+    // Travel up while the complete tree is not hepified.
+    // This is a O(Logn) loop
+    while (i && minHeap->array[i]->dist < minHeap->array[(i - 1) / 2]->dist)
+    {
+        minHeap->ups_decrease_key++;
+        // Swap this node with its parent
+        nodes[minHeap->array[i]->v].pos= (i-1)/2;
+        nodes[minHeap->array[(i-1)/2]->v].pos = i;
+        swapMinHeapNode(&minHeap->array[i], &minHeap->array[(i - 1) / 2]);
+ 
+        // move to parent index
+        i = (i - 1) / 2;
+    }
+}
+ 
+// A utility function to check if a given vertex
+// 'v' is in min heap or not
+bool isInMinHeap(struct MinHeap *minHeap, int v)
+{
+   if (nodes[v].pos < minHeap->size)
+     return true;
+   return false;
+}
+ 
